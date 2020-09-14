f5317bae81fd291810088df412ad90b705b9fb33
solmystez <53515169+solmystez@users.noreply.github.com>
2020-09-14 11:21:33 +0700

    Revert "Revert "1.4.5.3-""
    This reverts commit f97acf160a27007d60d50e07eec16227bbb942cc.

--- src/main/java/com/demo/lookopediaSinarmas/web/CartController.java
+++ src/main/java/com/demo/lookopediaSinarmas/web/CartController.java
@@ -35,26 +35,26 @@ public class CartController {
 	
 	//addProduct to invoice With ProductId
 	@PostMapping("/addProduct/{product_id}/{user_id}")
-	public ResponseEntity<?> addProductToCartOrAddQty(@Valid @RequestBody Orders invoice, BindingResult result,
+	public ResponseEntity<?> addProductToCartOrAddQty(@Valid @RequestBody Orders order, BindingResult result,
 			 							@PathVariable Long product_id, @PathVariable Long user_id){
 				
 		
 		ResponseEntity<?> mapError = mapValidationErrorService.MapValidationService(result);
 		if(mapError != null) return mapError;
 		
-		Orders invoice1 = cartService.addProductToCartOrAddQty(product_id, user_id, invoice);
+		Orders invoice1 = cartService.addProductToCartOrAddQty(product_id, user_id, order);
 		return new ResponseEntity<Orders>(invoice1, HttpStatus.CREATED);
 	}
 	
 	@PostMapping("/subProduct/{product_id}/{user_id}")
-	public ResponseEntity<?> subProductFromCart(@Valid @RequestBody Orders invoice, BindingResult result,
+	public ResponseEntity<?> subProductFromCart(@Valid @RequestBody Orders order, BindingResult result,
 			 							@PathVariable Long product_id, @PathVariable Long user_id){
 				
 		
 		ResponseEntity<?> mapError = mapValidationErrorService.MapValidationService(result);
 		if(mapError != null) return mapError;
 		
-		Orders invoice1 = cartService.subProductInCart(product_id, user_id, invoice);
+		Orders invoice1 = cartService.subProductInCart(product_id, user_id, order);
 		return new ResponseEntity<Orders>(invoice1, HttpStatus.CREATED);
 	}
 	
--- src/main/java/com/demo/lookopediaSinarmas/web/ProductController.java
+++ src/main/java/com/demo/lookopediaSinarmas/web/ProductController.java
@@ -1,29 +1,17 @@
 package com.demo.lookopediaSinarmas.web;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.demo.lookopediaSinarmas.domain.Category;
-import com.demo.lookopediaSinarmas.domain.Orders;
 import com.demo.lookopediaSinarmas.domain.Product;
-import com.demo.lookopediaSinarmas.domain.User;
-import com.demo.lookopediaSinarmas.services.CategoryService;
-import com.demo.lookopediaSinarmas.services.CommentService;
 import com.demo.lookopediaSinarmas.services.ProductService;
-import com.demo.lookopediaSinarmas.services.RatingService;
-import com.demo.lookopediaSinarmas.services.otherService.MapValidationErrorService;
 
 @CrossOrigin
 @RestController
@@ -33,15 +21,6 @@ public class ProductController {
 	@Autowired
 	private ProductService productService;
 	
-	@Autowired
-	private CommentService commentService;
-	
-	@Autowired
-	private RatingService ratingService;
-	
-	@Autowired
-	private MapValidationErrorService mapValidationErrorService;
-
 	@GetMapping("/findProductByCategory/{category_name}")
 	public Iterable<Product> loadMerchantProduct(@PathVariable String category_name){
 		return productService.findProductByCategory(category_name);
