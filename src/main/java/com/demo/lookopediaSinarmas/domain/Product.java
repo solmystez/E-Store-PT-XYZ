bf27975a8f4ac4ffa542e623b075cf351209d80b
solmystez <53515169+solmystez@users.noreply.github.com>
2020-09-15 20:58:07 +0700

    azure-server-apply

--- src/main/java/com/demo/lookopediaSinarmas/domain/Product.java
+++ src/main/java/com/demo/lookopediaSinarmas/domain/Product.java
@@ -43,6 +43,8 @@ public class Product {
 	
 	private String productImage;
 
+	@NotBlank(message = "Product category name is required !")
+	private String productCategoryName;
 	
 	@JsonFormat(pattern = "yyyy-mm-dd") 
 	private Date created_at;
@@ -85,8 +87,15 @@ public class Product {
 	private Merchant merchant;
 	
 	private String merchantName;
-	
-	
+		
+	public String getProductCategoryName() {
+		return productCategoryName;
+	}
+
+	public void setProductCategoryName(String productCategoryName) {
+		this.productCategoryName = productCategoryName;
+	}
+
 	public String getMerchantName() {
 		return merchantName;
 	}
