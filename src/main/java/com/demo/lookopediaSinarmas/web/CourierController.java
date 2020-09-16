a0d2680a7e3adb32a09f7a7e2bdbf5bc01516a32
solmystez <53515169+solmystez@users.noreply.github.com>
2020-09-16 15:58:52 +0700

    fixing-error + config postgras
    entity 'user' gbsa di pake di postgras
    harus jd 'users'

--- src/main/java/com/demo/lookopediaSinarmas/web/CourierController.java
+++ src/main/java/com/demo/lookopediaSinarmas/web/CourierController.java
@@ -1,5 +1,8 @@
 package com.demo.lookopediaSinarmas.web;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,13 +10,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.demo.lookopediaSinarmas.domain.Courier;
+import com.demo.lookopediaSinarmas.exceptions.ProductIdException;
+import com.demo.lookopediaSinarmas.repositories.CourierRepository;
 import com.demo.lookopediaSinarmas.services.CourierService;
 import com.demo.lookopediaSinarmas.services.otherService.MapValidationErrorService;
 
@@ -26,10 +34,13 @@ public class CourierController {
 	private CourierService courierService;
 	
 	@Autowired
+	private CourierRepository courierRepostory;
+	
+	@Autowired
 	private MapValidationErrorService mapValidationErrorService;
 	
 	@PostMapping("/create")
-	public ResponseEntity<?> addProductToCartOrAddQty(@Valid @RequestBody Courier courier, BindingResult result){
+	public ResponseEntity<?> createCourier(@Valid @RequestBody Courier courier, BindingResult result){
 		
 		ResponseEntity<?> mapError = mapValidationErrorService.MapValidationService(result);
 		if(mapError != null) return mapError;
@@ -42,4 +53,45 @@ public class CourierController {
 	public Iterable<Courier> loadAllProduct(){
 		return courierService.getCourierList();
 	}
+	
+	///////another variation
+	
+	//variation to create
+	@PostMapping("/testCreateCourier")
+	public Courier testCreateCourier(@RequestBody Courier courier) {
+		return this.courierRepostory.save(courier);
+	}
+	
+	//variation to update
+	@PutMapping("/update/{courier_id}")
+	public ResponseEntity<?> addProductToCartOrAddQty(@PathVariable(value = "courier_id") Long id,
+			@Valid @RequestBody Courier courierDetails, BindingResult result){
+		
+		ResponseEntity<?> mapError = mapValidationErrorService.MapValidationService(result);
+		if(mapError != null) return mapError;
+	
+		
+		Courier courier1 = courierRepostory.findById(id)
+				.orElseThrow(() -> new ProductIdException("Courier not found in this id :: " + id ));
+		
+		courier1.setCourierName(courierDetails.getCourierName());
+		courier1.setCourierPrice(courierDetails.getCourierPrice());
+		courier1.setCourierDescription(courierDetails.getCourierDescription());
+		
+		return ResponseEntity.ok(this.courierRepostory.save(courier1));
+	}
+	
+	@DeleteMapping("/delete/{courier_id}")
+	public Map<String, Boolean> deleteCourier(@PathVariable(value = "courier_id") Long id) {
+		Courier courier1 = courierRepostory.findById(id)
+				.orElseThrow(() -> new ProductIdException("Courier not found in this id :: " + id ));
+		
+		this.courierRepostory.delete(courier1);
+		
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		
+		return response;
+	}
+
 }
