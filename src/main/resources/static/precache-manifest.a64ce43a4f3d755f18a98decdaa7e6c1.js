1cfd83cd356618994117e1b66ea6e0f154c09cc2
solmystez <53515169+solmystez@users.noreply.github.com>
2020-10-13 09:55:18 +0700

    npm-build-test

---
+++ src/main/resources/static/precache-manifest.a64ce43a4f3d755f18a98decdaa7e6c1.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af82c1e34223e92c541f6c5d054f6a8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27d01cc5fe22be14213f",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "49cd18edb6cabc5b0f5b",
+    "url": "/static/css/main.15824665.chunk.css"
+  },
+  {
+    "revision": "27d01cc5fe22be14213f",
+    "url": "/static/js/2.b8b39596.chunk.js"
+  },
+  {
+    "revision": "190876527c22094e5338e41d900c9fc7",
+    "url": "/static/js/2.b8b39596.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49cd18edb6cabc5b0f5b",
+    "url": "/static/js/main.11af4e92.chunk.js"
+  },
+  {
+    "revision": "fadae01d5cc4f8c3881f",
+    "url": "/static/js/runtime-main.b3965851.js"
+  },
+  {
+    "revision": "e8d6a3688f1cc749edc8dccdf8762784",
+    "url": "/static/media/Banner1.e8d6a368.jpg"
+  },
+  {
+    "revision": "93c174a0139460efa241dc2c4f364972",
+    "url": "/static/media/Banner2.93c174a0.jpg"
+  },
+  {
+    "revision": "34f9833374cf8d2c1ccaf43466d12682",
+    "url": "/static/media/Banner3.34f98333.jpg"
+  },
+  {
+    "revision": "b7569f93ee5321a59817d8be9f0282b7",
+    "url": "/static/media/Banner4.b7569f93.jpg"
+  },
+  {
+    "revision": "5fc4a966a191a661e7043d5b29b8afcb",
+    "url": "/static/media/HeroImg.5fc4a966.png"
+  },
+  {
+    "revision": "b5a9716d166d83b9d8a429641196cd5a",
+    "url": "/static/media/HeroImgSU.b5a9716d.PNG"
+  },
+  {
+    "revision": "5c1926fb157d8db4e001f88198e2457b",
+    "url": "/static/media/all-card.5c1926fb.jpg"
+  },
+  {
+    "revision": "c5a064c65e90d99319004fc0e444e47e",
+    "url": "/static/media/clothes.c5a064c6.jpg"
+  },
+  {
+    "revision": "39853af521679156ee19e5e047a4adad",
+    "url": "/static/media/download-info.39853af5.png"
+  },
+  {
+    "revision": "f185b7f0eac9403d2d6479f11269dc1b",
+    "url": "/static/media/example-image.f185b7f0.jpg"
+  },
+  {
+    "revision": "1a4b965ece3edd39d4ae38d5004add91",
+    "url": "/static/media/food-card.1a4b965e.jpg"
+  },
+  {
+    "revision": "e1f13b8d2fa40bd250dd092690ead9e3",
+    "url": "/static/media/hero3.e1f13b8d.jpg"
+  },
+  {
+    "revision": "5264bd162f0a6480a8ab079a081c41dd",
+    "url": "/static/media/logo.5264bd16.png"
+  },
+  {
+    "revision": "57494e6bfd1d080da4d9dbd91d265e37",
+    "url": "/static/media/profileimage.57494e6b.png"
+  }
+]);
